dd84ee74c432c0d273381dde69c2055b7ab4b6f9
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2017-12-06 20:59:57 +0100

    add script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function getTriangleArea(a, h) {
+
+   	if(a > 0 && h > 0){
+   		return a*h/2;
+   	}
+
+   	else if(a <= 0 || h <= 0){}
+
+   	else {
+   		console.log("Nieprawidłowe dane");
+   	} 
+
+
+}
+
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(10, 6);
+var triangle2Area = getTriangleArea(10, 7);
+var triangle3Area = getTriangleArea(6, 15);
